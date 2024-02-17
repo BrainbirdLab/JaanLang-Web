d805c3e53953d3fe6ee85c89cfdc8b843f6b792a
Fuad Hasan <fuad.cs22@gmail.com>
2024-02-17 13:00:20 +0600

    PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,27 @@
     import "$lib/globasl.scss";
     import "$lib/fontawesome/css/all.min.css";
     import "highlight.js/styles/atom-one-dark.min.css";
+    import { showToastMessage } from "domtoastmessage";
+    import { onMount } from "svelte";
+
+    async function detectSWUpdate(){
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.addEventListener("updatefound", () => {
+            const newWorker = registration.installing;
+            newWorker?.addEventListener("statechange", () => {
+                if (newWorker.state === "installed") {
+                    newWorker.postMessage({ type: "SKIP_WAITING" });
+                    console.log("New update available");
+                    showToastMessage("App updated");
+                }
+            });
+        });
+    }
+
+    onMount(() => {
+        detectSWUpdate();
+    });
 
 </script>
 
